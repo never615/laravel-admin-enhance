07fa3ba1e511f98c1162f8d4c296d8d963734684
never615 <583531034@qq.com>
2018-04-28 11:43:30 +0800

    迁移修改:报表外键动作修改,uploads表外键动作修改

---
+++ migrations/2018_04_28_150050_update_reports_table2.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (c) 2017. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateReportsTable2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropForeign(["admin_user_id"]);
+            $table->foreign('admin_user_id')
+                ->references('id')
+                ->on('admin_users')
+                ->onDelete('SET NULL');
+            $table->unsignedInteger("admin_user_id")->nullable()->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+        });
+    }
+}
---
+++ migrations/2018_04_28_150052_update_uploads_table.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (c) 2017. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUploadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('uploads', function (Blueprint $table) {
+            $table->dropForeign(["admin_user_id"]);
+            $table->foreign('admin_user_id')
+                ->references('id')
+                ->on('admin_users')
+                ->onDelete('SET NULL');
+            $table->unsignedInteger("admin_user_id")->nullable()->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('uploads', function (Blueprint $table) {
+        });
+    }
+}
