96b17ae27cf19ffe858fc389acce63891e51339f
never615 <583531034@qq.com>
2018-05-11 13:35:02 +0800

    add footer

---
+++ resources/admin/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        {{--<a href="{!! config('app.wiki') !!}">帮助文档</a>--}}
+        {{--|--}}
+        {{--<a href="{!! config('app.wiki_version') !!}"><strong>Changelog</strong></a>--}}
+    </div>
+    <!-- Default to the left -->
+    <strong>Powered by <a href="https://mall-to.com" target="_blank">{{config("app.name")}}</a></strong>
+</footer>
+
