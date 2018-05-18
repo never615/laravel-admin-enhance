0303c188252ad2386dcef55fdae69eae28508b3a
never615 <583531034@qq.com>
2018-05-18 17:21:03 +0800

    管理端页脚处理

--- resources/admin/views/partials/footer.blade.php
+++ resources/admin/views/partials/footer.blade.php
@@ -2,9 +2,9 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        {{--<a href="{!! config('app.wiki') !!}">帮助文档</a>--}}
-        {{--|--}}
-        {{--<a href="{!! config('app.wiki_version') !!}"><strong>Changelog</strong></a>--}}
+        <a target="_blank" href="{!! config('app.wiki') !!}">帮助文档</a>
+        |
+        <a target="_blank" href="{!! config('app.wiki_version') !!}"><strong>Changelog</strong></a>
     </div>
     <!-- Default to the left -->
     <strong>Powered by <a href="https://mall-to.com" target="_blank">{{config("app.name")}}</a></strong>
