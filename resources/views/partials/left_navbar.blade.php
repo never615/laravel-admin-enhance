228cae98c7cf0c17a80e83a399e7686159aeb536
never615 <583531034@qq.com>
2018-06-14 18:26:17 +0800

    顶部导航快递访问按钮优化

--- resources/views/partials/left_navbar.blade.php
+++ resources/views/partials/left_navbar.blade.php
@@ -1,14 +1,21 @@
-<ul class="nav navbar-nav hidden-sm">
+<style>
+.quick-menu{
+    float: left;
+    margin: 0 0 0 5px;
+}
+.quick-menu-text{
+    font-size: 0.8em;
+}
+</style>
+<ul class="quick-menu nav navbar-nav hidden-sm">
     <li class="dropdown">
-        <a href="" class="dropdown-toggle btn btn-primary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-            <i class="fa fa-fw fa-plus visible-xs-inline-block"></i>
-            <span class="ng-scope">快捷访问</span> <span class="caret"></span>
-        </a>
+        <button class="dropdown-toggle btn btn-primary btn-lg" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <span class="ng-scope quick-menu-text">快捷访问</span> <span class="caret"></span>
+        </button>
         <ul class="dropdown-menu " role="menu" aria-labelledby="dropdownMenu1">
             @foreach ($speedy as $key=>$value)
                 <li><a href="{{$key}}" class="ng-scope">{{$value}}</a></li>
             @endforeach
-
         </ul>
     </li>
 </ul>
