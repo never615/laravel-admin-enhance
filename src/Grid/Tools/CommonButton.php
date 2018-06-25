fe2512dc654e857bebbacae61443902469c7ccc3
never615 <583531034@qq.com>
2018-06-25 17:15:37 +0800

    通用的grid按钮

---
+++ src/Grid/Tools/CommonButton.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright (c) 2018. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ */
+
+namespace Mallto\Admin\Grid\Tools;
+
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Grid\Tools\AbstractTool;
+
+class CommonButton extends AbstractTool
+{
+    private $name;
+    private $url;
+
+    /**
+     * CommonButton constructor.
+     *
+     * @param $name
+     * @param $url
+     */
+    public function __construct($name, $url)
+    {
+        $this->name = $name;
+        $this->url = $url;
+    }
+
+
+    /**
+     * Script for this tool.
+     *
+     * @return string
+     */
+    protected function script()
+    {
+        return <<<EOT
+
+
+    $('.mt-grid-common').on("click",function(){
+        var ids = selectedRows();
+        if (ids.length != 1) {
+            sweetAlert("需要选择一个用户");
+        }else{
+            var id = ids[0];
+            window.open("$this->url?user_id="+id,"_blank");
+        }
+    });
+
+
+EOT;
+    }
+
+
+    /**
+     * Render CreateButton.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        Admin::script($this->script());
+
+
+        return <<<EOT
+<a class="btn btn-sm btn-primary mt-grid-common" target="_blank"> $this->name</a>
+
+
+EOT;
+    }
+}
