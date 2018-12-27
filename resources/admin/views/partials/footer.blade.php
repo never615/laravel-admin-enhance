d5bd2b1849a744d0b64bc495b4d230305443bec0
never615 <583531034@qq.com>
2018-12-27 17:00:00 +0800

    页脚添加版本号

--- resources/admin/views/partials/footer.blade.php
+++ resources/admin/views/partials/footer.blade.php
@@ -3,8 +3,8 @@
     <!-- To the right -->
     <div class="pull-right hidden-xs">
         <a target="_blank" href="{!! config('app.wiki') !!}">帮助文档</a>
-        |
-        <a target="_blank" href="{!! config('app.change_log_url') !!}"><strong>Changelog</strong></a>
+        | <a target="_blank" href="{!! config('app.change_log_url') !!}"><strong>Changelog</strong></a>
+        | <strong>v {{config('app.version')}}</strong>
     </div>
     <!-- Default to the left -->
     <strong>Powered by <a href="https://mall-to.com" target="_blank">{{config("app.name")}}</a></strong>
