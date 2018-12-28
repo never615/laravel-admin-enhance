88c21532c1de325339fb6831cc74ceb995ac21cf
never615 <583531034@qq.com>
2018-12-28 11:29:35 +0800

    导入模块升级：支持自定义导入按钮的跳转链接；导入记录增加extra字段

---
+++ migrations/2018_12_28_150050_update_import_records_add_extra.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright (c) 2017. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * 导入记录
+ * Class CreateImportRecordsTable
+ */
+class UpdateImportRecordsAddExtra extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("import_records", function (\Illuminate\Database\Schema\Blueprint $table) {
+            $table->json("extra")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("import_records", function (\Illuminate\Database\Schema\Blueprint $table) {
+            $table->dropColumn("extra");
+        });
+    }
+}
