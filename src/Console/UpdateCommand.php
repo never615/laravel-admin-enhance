de473e15e15e1630389bc7ae0ecc835db1a6f7e2
never615 <583531034@qq.com>
2019-01-30 10:58:07 +0800

    add --force in seeder command

--- src/Console/UpdateCommand.php
+++ src/Console/UpdateCommand.php
@@ -39,7 +39,7 @@ class UpdateCommand extends Command
      */
     public function handle()
     {
-        $this->call('db:seed', ['--class' => TablesSeeder::class]);
+        $this->call('db:seed', ['--class' => TablesSeeder::class, '--force' => true]);
     }
 
 }
