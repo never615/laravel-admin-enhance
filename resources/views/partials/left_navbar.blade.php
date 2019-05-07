e414d846f25e780b4f8e65fce96c70a3e8c94fba
never615 <583531034@qq.com>
2019-05-07 18:09:17 +0800

    顶部导航中的快捷访问样式优化

--- resources/views/partials/left_navbar.blade.php
+++ resources/views/partials/left_navbar.blade.php
@@ -1,15 +1,20 @@
 <style>
 .quick-menu{
     float: left;
-    margin: 0 0 0 5px;
+}
+
+.quick-menu .dropdown button{
+    height: 40px;
+    padding: 10px;
+    margin: 5px;
 }
 .quick-menu-text{
-    font-size: 0.8em;
+    /*font-size: 0.8em;*/
 }
 </style>
 <ul class="quick-menu nav navbar-nav hidden-sm">
     <li class="dropdown">
-        <button class="dropdown-toggle btn btn-primary btn-lg" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <button class="dropdown-toggle btn btn-primary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="ng-scope quick-menu-text">快捷访问</span> <span class="caret"></span>
         </button>
         <ul class="dropdown-menu " role="menu" aria-labelledby="dropdownMenu1">
