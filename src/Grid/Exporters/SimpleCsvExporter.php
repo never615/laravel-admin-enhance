eacb0eca896aee0f7a3a9de36afe460abcffaca8
never615 <583531034@qq.com>
2019-08-20 16:53:44 +0800

    wip

--- src/Grid/Exporters/SimpleCsvExporter.php
+++ src/Grid/Exporters/SimpleCsvExporter.php
@@ -21,12 +21,11 @@ abstract class SimpleCsvExporter extends CsvExporter
 {
 
     /**
-     * 默认移除一些key开启
+     * 是否默认移除一些key
      *
      * @var bool
      */
     protected $forgetKeysDefault = true;
-
 
     /**
      * 一般,当数据库字段保存的是json数据时,需要在此设置该字段忽略,避免json数据被错误转换(被array_dot()转成一维数组)
@@ -72,6 +71,11 @@ abstract class SimpleCsvExporter extends CsvExporter
      *
      * 参数可以传入关联数据的**模型名**来忽略该模型下的全部数据,
      * 如导出user数据的时候,传入member会忽略user关联的member对象下的所有字段.
+     *
+     * 也可以使用member.name移除关联模型的指定字段
+     *
+     *
+     * 默认移除了一些字段参见  @var $this->defaultForgetKeys
      *
      * @return array
      */
