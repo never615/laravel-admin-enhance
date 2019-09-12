4f45540bfa85e96d2d797902e578e7b6099d40a7
never615 <never615@gmail.com>
2019-09-12 19:16:56 +0800

    检查管理端登录账号的新增/删除/详情页查看权限. 如果是主体账号禁止管理管理范围之外店铺数据. 如果是店铺账号,直接禁止新增和删除权限.

--- src/Controllers/Base/AdminDataFilterTrait.php
+++ src/Controllers/Base/AdminDataFilterTrait.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Copyright (c) 2018. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
+ * Copyright (c) 2019. Mallto.Co.Ltd.<mall-to.com> All rights reserved.
  */
 
 namespace Mallto\Admin\Controllers\Base;
-
 
 use Encore\Admin\Facades\Admin;
 use Illuminate\Support\Facades\Schema;
@@ -13,22 +12,96 @@ use Mallto\Admin\SubjectUtils;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
- * 处理管理端表格和表单数据展示的过滤
- * Class AdminFilterData
- *
- * @package Mallto\Admin\Controllers\Base
+ * User: never615 <never615.com>
+ * Date: 2019-09-12
+ * Time: 17:01
  */
-trait AdminFilterData
+trait AdminDataFilterTrait
 {
 
     /**
+     * 列表也数据过滤权限检查
+     *
+     * @param $grid
+     */
+    protected function indexFilter($grid)
+    {
+        $this->indexFilterDataBySubject($grid);
+    }
+
+
+    /**
+     * 创建页面权限检查
+     */
+    protected function createFilter()
+    {
+
+    }
+
+    /**
+     * 处理进入详情页/更新数据/删除数据的权限
+     *
+     * @param $id
+     */
+    protected function formFilterById($id)
+    {
+        $this->formFilterDataForSubjectById($id);
+    }
+
+
+    /**
+     * 保存数据检查
+     */
+    protected function storeFilter()
+    {
+
+    }
+
+    //=========== 以上几个方法涵盖了增删改查各个纬度,一般重写以上方法即可  =================
+
+    /**
+     * 编辑页权限检查
+     *
+     * @param $id
+     */
+    protected function editFilter($id)
+    {
+        $this->formFilterById($id);
+    }
+
+
+    /**
+     * 更新数据
+     *
+     * @param $id
+     */
+    protected function updateFilter($id)
+    {
+        $this->formFilterById($id);
+    }
+
+
+    /**
+     * 删除数据动作权限检查
+     *
+     * @param $id
+     */
+    protected function destroyFilter($id)
+    {
+        $this->formFilterById($id);
+    }
+
+
+    /**
      * 过滤grid显示下的数据
+     *
+     * 处理subject纬度的数据
      *
      * 支持subject父子关系查看数据
      *
      * @param $grid
      */
-    protected function gridFilterData($grid)
+    protected function indexFilterDataBySubject($grid)
     {
         $adminUser = Admin::user();
 
@@ -77,15 +150,16 @@ trait AdminFilterData
         }
     }
 
-
     /**
-     * 编辑模式下阻止没有数据查看权限的操作
+     * 处理subject纬度阻止没有数据权限的操作
+     *
+     * @param $id
      */
-    protected function editFilterData()
+    protected function formFilterDataForSubjectById($id)
     {
         $model = resolve($this->getModel());
         //检查记录是否已经删除
-        $obj = $model::find($this->currentId);
+        $obj = $model::find($id);
         if (!$obj) {
             throw new HttpException(422, "记录不存在或已经删除");
         }
@@ -129,14 +203,16 @@ trait AdminFilterData
             $tableName = $model->getTable();
             if ($tableName == "subjects") {
                 //如果访问的subject的id属于$subjectIds可以访问
-                if (!in_array($this->currentId, $subjectIds)) {
+                if (!in_array($id, $subjectIds)) {
                     throw new HttpException(403, "没有权限查看");
                 }
             } elseif (Schema::hasColumn($tableName, "subject_id")) {
-                if (!$model->whereIn('subject_id', $subjectIds)->where('id', $this->currentId)->exists()) {
+                if (!$model->whereIn('subject_id', $subjectIds)->where('id', $id)->exists()) {
                     throw new HttpException(403, "没有权限查看");
                 }
             }
         }
     }
+
+
 }
