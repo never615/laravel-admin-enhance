80953c2a56b579c7ae21b47c6f091605638d5bfa
never615 <never615@gmail.com>
2019-12-17 23:58:04 +0800

    fix bug

--- src/Data/Administrator.php
+++ src/Data/Administrator.php
@@ -6,12 +6,7 @@
 namespace Mallto\Admin\Data;
 
 
-use Encore\Admin\Traits\AdminBuilder;
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Model;
 use Mallto\Admin\Data\Traits\DynamicData;
-use Mallto\Admin\Data\Traits\HasPermissions;
 use Mallto\Admin\Data\Traits\SelectSource;
 use SMartins\PassportMultiauth\HasMultiAuthApiTokens;
 
@@ -20,9 +15,9 @@ use SMartins\PassportMultiauth\HasMultiAuthApiTokens;
  *
  * @property Role[] $roles
  */
-class Administrator extends Model implements AuthenticatableContract
+class Administrator extends \Encore\Admin\Auth\Database\Administrator
 {
-    use Authenticatable, AdminBuilder, HasPermissions, DynamicData, HasMultiAuthApiTokens, SelectSource;
+    use DynamicData, HasMultiAuthApiTokens, SelectSource;
 
 
     const STATUS = [
@@ -36,15 +31,10 @@ class Administrator extends Model implements AuthenticatableContract
     ];
 
     protected $fillable = [
-        'username',
-        'password',
-        'name',
-        'avatar',
-        "subject_id",
-        "adminable_id",
-        "adminable_type",
-        "manager_subject_ids",
     ];
+
+
+    protected $guarded = [];
 
     protected $casts = [
         'extra'               => 'array',
@@ -52,21 +42,6 @@ class Administrator extends Model implements AuthenticatableContract
         'openid'              => 'array', //用户微信信息
     ];
 
-    /**
-     * Create a new Eloquent model instance.
-     *
-     * @param array $attributes
-     */
-    public function __construct(array $attributes = [])
-    {
-        $connection = config('admin.database.connection') ?: config('database.default');
-
-        $this->setConnection($connection);
-
-        $this->setTable(config('admin.database.users_table'));
-
-        parent::__construct($attributes);
-    }
 
     public function subject()
     {
