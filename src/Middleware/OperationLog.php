e5c73ed0e6b11be30cbf7d9ac345e31cb8071222
never615 <never615@gmail.com>
2020-03-08 13:26:41 +0800

    fix bug

--- src/Middleware/OperationLog.php
+++ src/Middleware/OperationLog.php
@@ -32,68 +32,69 @@ class OperationLog extends LogOperation
      */
     public function handle(Request $request, \Closure $next)
     {
-
-        if ($this->shouldLogOperation($request)) {
-            $adminUser = null;
-
-            try {
-                $adminUser = Admin::user();
-                if ( ! $adminUser && ! empty(config('auth.guards.admin_api'))) {
-                    $adminUser = Auth::guard("admin_api")->user();
-                }
-            } catch (\Exception $exception) {
-
-            }
-
-            $ip = 0;
-            $tempIp = $request->header("X-Forwarded-For");
-            if ($tempIp) {
-                $ip = $tempIp;
-            } else {
-                $ip = $request->getClientIp();
-            }
-
-            $log = [
-                'uuid'       => SubjectUtils::getUUIDNoException() ?: 0,
-                'user_id'    => $adminUser->id ?? 0,
-                'path'       => $request->path(),
-                'method'     => $request->method(),
-                'request_ip' => $ip,
-                'input'      => json_encode($request->all(), JSON_UNESCAPED_UNICODE),
-                'header'     => json_encode($request->headers->all(), JSON_UNESCAPED_UNICODE),
-                'subject_id' => $adminUser->subject->id ?? 0,
-                "action"     => "request",
-            ];
-
-            dispatch(new LogJob("logAdminOperation", $log));
-
-            $response = $next($request);
-            $content = $response->getContent();
-
-            if (is_array($content)) {
-                $input = json_encode($content);
-            } else {
-                if ( ! is_string($content)) {
-                    $input = "其他数据";
-                } else {
-                    $input = $content;
-                }
-            }
-
-            $log = [
-                'uuid'       => SubjectUtils::getUUIDNoException() ?: 0,
-                'user_id'    => $adminUser->id ?? 0,
-                'path'       => $request->path(),
-                'method'     => $request->method(),
-                'request_ip' => $ip,
-                'input'      => $input,
-                'subject_id' => $adminUser->subject->id ?? 0,
-                'action'     => "response",
-                'status'     => $response->getStatusCode(),
-            ];
-
-            dispatch(new LogJob("logAdminOperation", $log));
+        if ( ! $this->shouldLogOperation($request)) {
+            return $next($request);
         }
+
+        $adminUser = null;
+
+        try {
+            $adminUser = Admin::user();
+            if ( ! $adminUser && ! empty(config('auth.guards.admin_api'))) {
+                $adminUser = Auth::guard("admin_api")->user();
+            }
+        } catch (\Exception $exception) {
+
+        }
+
+        $ip = 0;
+        $tempIp = $request->header("X-Forwarded-For");
+        if ($tempIp) {
+            $ip = $tempIp;
+        } else {
+            $ip = $request->getClientIp();
+        }
+
+        $log = [
+            'uuid'       => SubjectUtils::getUUIDNoException() ?: 0,
+            'user_id'    => $adminUser->id ?? 0,
+            'path'       => $request->path(),
+            'method'     => $request->method(),
+            'request_ip' => $ip,
+            'input'      => json_encode($request->all(), JSON_UNESCAPED_UNICODE),
+            'header'     => json_encode($request->headers->all(), JSON_UNESCAPED_UNICODE),
+            'subject_id' => $adminUser->subject->id ?? 0,
+            "action"     => "request",
+        ];
+
+        dispatch(new LogJob("logAdminOperation", $log));
+
+        $response = $next($request);
+        $content = $response->getContent();
+
+        if (is_array($content)) {
+            $input = json_encode($content);
+        } else {
+            if ( ! is_string($content)) {
+                $input = "其他数据";
+            } else {
+                $input = $content;
+            }
+        }
+
+        $log = [
+            'uuid'       => SubjectUtils::getUUIDNoException() ?: 0,
+            'user_id'    => $adminUser->id ?? 0,
+            'path'       => $request->path(),
+            'method'     => $request->method(),
+            'request_ip' => $ip,
+            'input'      => $input,
+            'subject_id' => $adminUser->subject->id ?? 0,
+            'action'     => "response",
+            'status'     => $response->getStatusCode(),
+        ];
+
+        dispatch(new LogJob("logAdminOperation", $log));
 
         return $response;
     }
