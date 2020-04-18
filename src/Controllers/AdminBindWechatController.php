2ba426bed3d5064995f28415a1e23357e6c9a4cb
never615 <never615@gmail.com>
2020-04-18 18:35:39 +0800

    增加wechat uuid配置

--- src/Controllers/AdminBindWechatController.php
+++ src/Controllers/AdminBindWechatController.php
@@ -38,7 +38,7 @@ class AdminBindWechatController extends Controller
 
         $subjecct = $waiteBindAdminUser->subject;
         $wechatUserInfo = $wechatUsecase->getUserInfo(SubjectUtils::getConfigByOwner(SubjectConfigConstants::OWNER_CONFIG_ADMIN_WECHAT_UUID,
-            $subjecct, $subjecct->uuid), $openid);
+            $subjecct, $subjecct), $openid);
 
         if ( ! $wechatUserInfo) {
             throw new ResourceException("未找到相应微信用户");
