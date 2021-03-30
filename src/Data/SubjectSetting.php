e533e4db857bccdfc7a234614721480cd69790ed
liangguifeng <1476982312@qq.com>
2021-03-30 12:22:23 +0800

    fix bug
    Signed-off-by: liangguifeng <1476982312@qq.com>

--- src/Data/SubjectSetting.php
+++ src/Data/SubjectSetting.php
@@ -16,7 +16,6 @@ class SubjectSetting extends BaseModel
         'public_configs'        => 'json',
         'private_config'        => 'json',
         'subject_owner_configs' => 'json',
-        'allow_pay_type'        => 'json',
     ];
 
     public function subjectConfigs()
