6e8b2166d779dc862867d9fdf7c6fdccaa68ccfc
never615 <never615@gmail.com>
2022-03-20 10:50:07 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,7 @@ Route::group($attributes, function ($router) {
         /**
          * 需要经过签名校验
          */
-        Route::group([ 'middleware' => [ 'authSign' ] ], function () {
+        Route::group([ 'middleware' => [ 'authSign_referrer' ] ], function () {
 
         });
 
--- src/SubjectSettingUtils.php
+++ src/SubjectSettingUtils.php
@@ -7,6 +7,7 @@ namespace Mallto\Admin;
 
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Schema;
 use Mallto\Admin\Data\SubjectSetting;
 use Mallto\Admin\Exception\NotSettingBySubjectOwnerException;
 use Mallto\Admin\Exception\SubjectNotFoundException;
@@ -23,7 +24,7 @@ class SubjectSettingUtils
      * 获取相关主体的项目配置
      *
      * @param      $key
-     * @param null $subject
+     * @param null $subject |$subjectId
      * @param null $default
      *
      * @return mixed|null
@@ -43,21 +44,35 @@ class SubjectSettingUtils
             }
         }
 
-        $subjectId = $subject->id;
+        if (is_numeric($subject)) {
+            $subjectId = $subject;
+        } else {
+            $subjectId = $subject->id;
+        }
 
         $value = Cache::store('memory')->get(SubjectSetting::getCacheKey($subjectId) . $key);
         if ( ! isset($value) || is_null($value)) {
-            $subjectSetting = SubjectSetting::query()
-                ->select([ $key, 'public_configs', 'private_configs', 'subject_owner_configs' ])
-                ->where('subject_id', $subject->id)
-                ->first();
+            if (Schema::hasColumn('subject_settings', $key)) {
+                $subjectSetting = SubjectSetting::query()
+                    ->select([ $key ])
+                    ->where('subject_id', $subjectId)
+                    ->first();
+            } else {
+                $subjectSetting = SubjectSetting::query()
+                    ->select([ 'public_configs', 'private_configs', 'subject_owner_configs' ])
+                    ->where('subject_id', $subjectId)
+                    ->first();
+            }
 
             $value = null;
             if ($subjectSetting) {
-                $value = $subjectSetting->$key
-                    ?? $subjectSetting['public_configs'][$key]
-                    ?? $subjectSetting['private_configs'][$key]
-                    ?? $subjectSetting['subject_owner_configs'][$key];
+                if (Schema::hasColumn('subject_settings', $key)) {
+                    $value = $subjectSetting->$key;
+                } else {
+                    $value = $subjectSetting['public_configs'][$key]
+                        ?? $subjectSetting['private_configs'][$key]
+                        ?? $subjectSetting['subject_owner_configs'][$key];
+                }
             }
 
             if ( ! is_null($value && ! empty($value))) {
@@ -75,6 +90,6 @@ class SubjectSettingUtils
             return $default;
         }
 
-        throw new NotSettingBySubjectOwnerException($key . "未配置," . $subject->id);
+        throw new NotSettingBySubjectOwnerException($key . "未配置," . $subjectId);
     }
 }
