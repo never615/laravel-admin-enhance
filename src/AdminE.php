0303d7522e5d4c3ba5d77a027241d196396ad319
never615 <never615@gmail.com>
2022-08-23 11:04:04 +0800

    fix bug

--- src/AdminE.php
+++ src/AdminE.php
@@ -143,37 +143,37 @@ class AdminE
      *
      * @return void
      */
-    public
-    function registerAuthRoutes()
-    {
-
-        $attributes = [
-            'prefix'     => config('admin.route.prefix'),
-            'middleware' => [ 'adminE_base' ],
-        ];
-
-        Route::group($attributes, function ($router) {
-            $attributes = [ 'middleware' => [ 'adminE.auto_permission' ] ];
-            /* @var \Illuminate\Routing\Router $router */
-            $router->group([ $attributes ], function ($router) {
-
-                /* @var \Illuminate\Routing\Router $router */
-                $router->resource('auth/admins', '\Mallto\Admin\Controllers\UserController');
-                $router->resource('auth/roles', '\Mallto\Admin\Controllers\RoleController');
-                $router->resource('auth/permissions', '\Mallto\Admin\Controllers\PermissionController');
-                $router->resource('auth/menus', '\Mallto\Admin\Controllers\MenuController',
-                    [ 'except' => [ 'create' ] ]);
-                $router->resource('auth/logs', '\Encore\Admin\Controllers\LogController',
-                    [ 'only' => [ 'index', 'destroy' ] ]);
-            });
-
-            $router->get('auth/login', '\Encore\Admin\Controllers\AuthController@getLogin');
-            $router->post('auth/login', '\Encore\Admin\Controllers\AuthController@postLogin');
-            $router->get('auth/logout', '\Encore\Admin\Controllers\AuthController@getLogout');
-            $router->get('auth/setting', '\Encore\Admin\Controllers\AuthController@getSetting');
-            $router->put('auth/setting', '\Encore\Admin\Controllers\AuthController@putSetting');
-        });
-    }
+    //public
+    //function registerAuthRoutes()
+    //{
+    //
+    //    $attributes = [
+    //        'prefix'     => config('admin.route.prefix'),
+    //        'middleware' => [ 'adminE_base' ],
+    //    ];
+    //
+    //    Route::group($attributes, function ($router) {
+    //        $attributes = [ 'middleware' => [ 'adminE.auto_permission' ] ];
+    //        /* @var \Illuminate\Routing\Router $router */
+    //        $router->group([ $attributes ], function ($router) {
+    //
+    //            /* @var \Illuminate\Routing\Router $router */
+    //            $router->resource('auth/admins', '\Mallto\Admin\Controllers\UserController');
+    //            $router->resource('auth/roles', '\Mallto\Admin\Controllers\RoleController');
+    //            $router->resource('auth/permissions', '\Mallto\Admin\Controllers\PermissionController');
+    //            $router->resource('auth/menus', '\Mallto\Admin\Controllers\MenuController',
+    //                [ 'except' => [ 'create' ] ]);
+    //            $router->resource('auth/logs', '\Encore\Admin\Controllers\LogController',
+    //                [ 'only' => [ 'index', 'destroy' ] ]);
+    //        });
+    //
+    //        $router->get('auth/login', '\Encore\Admin\Controllers\AuthController@getLogin');
+    //        $router->post('auth/login', '\Encore\Admin\Controllers\AuthController@postLogin');
+    //        $router->get('auth/logout', '\Encore\Admin\Controllers\AuthController@getLogout');
+    //        $router->get('auth/setting', '\Encore\Admin\Controllers\AuthController@getSetting');
+    //        $router->put('auth/setting', '\Encore\Admin\Controllers\AuthController@putSetting');
+    //    });
+    //}
 
 
     public function adminBootstrap()
