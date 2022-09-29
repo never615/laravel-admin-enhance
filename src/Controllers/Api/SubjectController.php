32c1ee17501d3729ce52d0aa5f96171cfb16fd18
never615 <never615@gmail.com>
2022-09-29 10:54:53 +0800

    wip

--- src/Controllers/Api/SubjectController.php
+++ src/Controllers/Api/SubjectController.php
@@ -6,7 +6,8 @@
 namespace Mallto\Admin\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Mallto\Admin\Data\Subject;
+use Illuminate\Http\Request;
+use Mallto\Mall\SubjectConfigConstants;
 
 /**
  * User: never615 <never615.com>
@@ -21,9 +22,20 @@ class SubjectController extends Controller
      *
      * @return mixed
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Subject::whereNotNull('uuid')->select('name', 'uuid')->get();
+        $projectType = $request->get('project_type');
+
+        $query = \Mallto\Mall\Data\Subject::query()
+            ->orderBy('created_at', 'desc')
+            ->whereNotNull('uuid')
+            ->select('name', 'uuid');
+
+        if ($projectType) {
+            $query->where('extra_config->' . SubjectConfigConstants::OWNER_CONFIG_PROJECT_TYPE, $projectType);
+        }
+
+        return $query->get();
     }
 
 }
