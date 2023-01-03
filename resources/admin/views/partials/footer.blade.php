cd6b0f18251439b38a38b21163a35278929170db
王荣 <never615@gmail.com>
2023-01-03 18:40:43 +0800

    no message

--- resources/admin/views/partials/footer.blade.php
+++ resources/admin/views/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <!-- Main Footer -->
 <footer class="main-footer">
     <!-- To the right -->
-{{--    <div class="pull-right hidden-xs">--}}
-{{--        @if(config('app.wiki'))--}}
-{{--            <a target="_blank"--}}
-{{--               href="{!! \Mallto\Admin\SubjectUtils::getDynamicKeyConfigByOwner("wiki", null, config("app.wiki"))!!}">帮助文档</a>@endif--}}
+    <div class="pull-right hidden-xs">
+        @if(config('app.wiki'))
+            <a target="_blank"
+               href="{!! \Mallto\Admin\SubjectUtils::getDynamicKeyConfigByOwner("wiki", null, config("app.wiki"))!!}">帮助文档</a>@endif
 {{--        @if(config('app.change_log_url'))| <a target="_blank" href="{!! config('app.change_log_url') !!}"><strong>Changelog</strong></a>@endif--}}
 {{--        | <strong>v {{config('app.version')}}</strong>--}}
-{{--    </div>--}}
+    </div>
     <!-- Default to the left -->
 {{--    <strong>Powered by <a href="https://mall-to.com" target="_blank">{{config("app.name")}}</a></strong>--}}
 </footer>
