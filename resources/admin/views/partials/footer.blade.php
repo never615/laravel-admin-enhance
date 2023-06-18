a2bcaacf9c5c45e9c1abf893fe708e9f38803f60
王荣 <never615@gmail.com>
2023-06-19 03:01:38 +0800

    管理端右下角显示版本号

--- resources/admin/views/partials/footer.blade.php
+++ resources/admin/views/partials/footer.blade.php
@@ -4,11 +4,12 @@
     <div class="pull-right hidden-xs">
         @if(config('app.wiki'))
             <a target="_blank"
-               href="{!! \Mallto\Admin\SubjectUtils::getDynamicKeyConfigByOwner("wiki", null, config("app.wiki"))!!}">帮助文档</a>@endif
-{{--        @if(config('app.change_log_url'))| <a target="_blank" href="{!! config('app.change_log_url') !!}"><strong>Changelog</strong></a>@endif--}}
-{{--        | <strong>v {{config('app.version')}}</strong>--}}
+               href="{!! \Mallto\Admin\SubjectUtils::getDynamicKeyConfigByOwner("wiki", null, config("app.wiki"))!!}">帮助文档</a>
+        @endif
+        {{--        @if(config('app.change_log_url'))| <a target="_blank" href="{!! config('app.change_log_url') !!}"><strong>Changelog</strong></a>@endif--}}
+        | <strong>v {{config('app.version')}}</strong>
     </div>
     <!-- Default to the left -->
-{{--    <strong>Powered by <a href="https://mall-to.com" target="_blank">{{config("app.name")}}</a></strong>--}}
+    {{--    <strong>Powered by <a href="https://mall-to.com" target="_blank">{{config("app.name")}}</a></strong>--}}
 </footer>
 
