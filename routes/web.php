bcdf5cf30cf717d974f4684d92b60bec60f4cbe2
王荣 <never615@gmail.com>
2023-10-12 18:15:21 +0800

    添加注册功能;优化主体管理归属管理;

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::group([
     'namespace'  => 'Mallto\Admin\Controllers\Admin\Api',
 ], function ($router) {
 
-    $router->post('auth/login', 'AuthController@postLogin');
+    //$router->post('auth/login', 'AuthController@postLogin');
     $router->get('auth/yzm', 'AuthController@captcha');
 
     Route::group([
@@ -56,75 +56,77 @@ Admin::routes();
 Route::group([
     'namespace'  => 'Mallto\Admin\Controllers',
     'middleware' => [ 'web' ],
+    'prefix'     => config('admin.route.prefix'),
 ], function ($router) {
 
     //todo 这个权限暂时放在这
-    Route::get('admin/admin_bind_wechat', 'AdminBindWechatController@bindWechat');
-    Route::get('admin/admin_unbind_wechat', 'AdminBindWechatController@unbindWechat');
-    $router->post('admin/auth/send_sms', 'AuthController@sendSms');
+    Route::get('admin_bind_wechat', 'AdminBindWechatController@bindWechat');
+    Route::get('admin_unbind_wechat', 'AdminBindWechatController@unbindWechat');
+    $router->post('auth/send_sms', 'AuthController@sendSms');
+
+    $router->get('auth/register', 'Admin\RegisterController@getRegister');
+    $router->post('auth/register', 'Admin\RegisterController@postRegister');
 
 //----------------------------------------  管理端开始  -----------------------------------------------
-    Route::group([ 'prefix' => config('admin.route.prefix'), 'middleware' => 'adminE_base' ],
-        function ($router) {
-            $router->get('/', 'HomeController@index')->name('dashboard');
+    Route::group([ 'middleware' => 'adminE_base' ], function ($router) {
+        $router->get('/', 'HomeController@index')->name('dashboard');
 
-            //获取七牛upload token
-            $router->get('uptoken', 'FileController@getUploadToken');
-            //上传图片(富文本编辑器需要使用)
-            $router->post('upload', 'FileController@upload');
+        //获取七牛upload token
+        $router->get('uptoken', 'FileController@getUploadToken');
+        //上传图片(富文本编辑器需要使用)
+        $router->post('upload', 'FileController@upload');
 
-            $router->get('auth/login', '\Encore\Admin\Controllers\AuthController@getLogin');
+        //$router->get('auth/login', '\Encore\Admin\Controllers\AuthController@getLogin');
 //            $router->post('auth/login', '\Encore\Admin\Controllers\AuthController@postLogin');
-            $router->post('auth/login', 'AuthController@postLogin');
+        $router->post('auth/login', 'AuthController@postLogin');
 
-            $router->get('auth/logout', '\Encore\Admin\Controllers\AuthController@getLogout');
-            $router->get('auth/setting', '\Encore\Admin\Controllers\AuthController@getSetting');
-            $router->put('auth/setting', '\Encore\Admin\Controllers\AuthController@putSetting');
+        //$router->get('auth/logout', '\Encore\Admin\Controllers\AuthController@getLogout');
+        //$router->get('auth/setting', '\Encore\Admin\Controllers\AuthController@getSetting');
+        //$router->put('auth/setting', '\Encore\Admin\Controllers\AuthController@putSetting');
 
-            Route::get('select_data/{key}', 'SelectSourceController@dataSource');
+        Route::get('select_data/{key}', 'SelectSourceController@dataSource');
 
-            Route::group([ 'middleware' => [ 'adminE.auto_permission' ] ], function ($router) {
-                $router->resource('auth/admins', '\Mallto\Admin\Controllers\UserController');
-                $router->resource('auth/roles', '\Mallto\Admin\Controllers\RoleController');
-                $router->resource('auth/permissions', '\Mallto\Admin\Controllers\PermissionController');
-                $router->resource('auth/menus', '\Mallto\Admin\Controllers\MenuController',
-                    [ 'except' => [ 'create' ] ]);
-                //$router->resource('auth/logs', '\Encore\Admin\Controllers\LogController',
-                //    [ 'only' => [ 'index', 'destroy' ] ]);
+        Route::group([ 'middleware' => [ 'adminE.auto_permission' ] ], function ($router) {
+            $router->resource('auth/admins', '\Mallto\Admin\Controllers\UserController');
+            $router->resource('auth/roles', '\Mallto\Admin\Controllers\RoleController');
+            $router->resource('auth/permissions', '\Mallto\Admin\Controllers\PermissionController');
+            $router->resource('auth/menus', '\Mallto\Admin\Controllers\MenuController', [ 'except' => [ 'create' ] ]);
+            //$router->resource('auth/logs', '\Encore\Admin\Controllers\LogController',
+            //    [ 'only' => [ 'index', 'destroy' ] ]);
 
-                $router->resource('subjects', 'SubjectController');
+            $router->resource('subjects', 'SubjectController');
 
-                $router->resource('reports', 'ReportController');
-                $router->resource('uploads', 'UploadController');
-                $router->resource('videos', 'VideoController');
+            $router->resource('reports', 'ReportController');
+            $router->resource('uploads', 'UploadController');
+            $router->resource('videos', 'VideoController');
 
-                //主体配置管理: 动态配置
-                $router->resource('subject_configs', 'SubjectConfigController');
+            //主体配置管理: 动态配置
+            $router->resource('subject_configs', 'SubjectConfigController');
 
-                //主体配置管理:一个表中的一行数据配置一个主体
-                Route::resource('subject_settings', 'SubjectSettingController');
+            //主体配置管理:一个表中的一行数据配置一个主体
+            Route::resource('subject_settings', 'SubjectSettingController');
 
-                //文件导入模块
-                $router->resource('import_settings', 'Import\ImportSettingController');
-                //导入记录
-                $router->resource('import_records', 'Import\ImportRecordController');
+            //文件导入模块
+            $router->resource('import_settings', 'Import\ImportSettingController');
+            //导入记录
+            $router->resource('import_records', 'Import\ImportRecordController');
 
-                //账户分组
-                Route::resource('admin_user_groups', 'AdminUserGroupController');
+            //账户分组
+            Route::resource('admin_user_groups', 'AdminUserGroupController');
 
-                Route::group([
-                    'namespace' => 'Admin',
-                ], function ($router) {
+            Route::group([
+                'namespace' => 'Admin',
+            ], function ($router) {
 
-                    //操作日志字典
-                    Route::resource("operation_log_dictionarys", 'OperationLogDictionaryController');
-                    Route::resource("auth/logs", 'OperationLogController');
+                //操作日志字典
+                Route::resource("operation_log_dictionarys", 'OperationLogDictionaryController');
+                Route::resource("auth/logs", 'OperationLogController');
 
-                    //登录用户管理
-                    Route::resource("login_users", 'LoginUserController');
-                });
+                //登录用户管理
+                Route::resource("login_users", 'LoginUserController');
             });
         });
+    });
 
 //----------------------------------------  管理端结束  -----------------------------------------------
 
