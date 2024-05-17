b7b91b9c82f7c5b820952b364274d30405df100d
never615 <never615@gmail.com>
2024-05-18 02:43:25 +0800

    wip

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,8 +6,6 @@
 namespace Mallto\Admin;
 
 use Doctrine\DBAL\DBALException;
-use Doctrine\DBAL\Types\FloatType;
-use Doctrine\DBAL\Types\Type;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
@@ -28,10 +26,10 @@ class ServiceProvider extends BaseServiceProvider
      * @var array
      */
     protected $commands = [
-            'Mallto\Admin\Console\InstallCommand',
-            'Mallto\Admin\Console\UpdateCommand',
-            'Mallto\Admin\Console\PathGeneratorCommand',
-            'Mallto\Admin\Console\MenuCommand',
+        'Mallto\Admin\Console\InstallCommand',
+        'Mallto\Admin\Console\UpdateCommand',
+        'Mallto\Admin\Console\PathGeneratorCommand',
+        'Mallto\Admin\Console\MenuCommand',
     ];
 
     /**
@@ -40,11 +38,11 @@ class ServiceProvider extends BaseServiceProvider
      * @var array
      */
     protected $routeMiddleware = [
-            'admin.auth' => \Mallto\Admin\Middleware\Authenticate::class,
-            'adminE.auto_permission' => \Mallto\Admin\Middleware\AutoPermissionMiddleware::class,
-            'adminE.log' => \Mallto\Admin\Middleware\OperationLog::class,
-            'adminE.pjax' => Pjax::class,
-            'adminE.replacement_password' => \Mallto\Admin\Middleware\ReplacementPassword::class,
+        'admin.auth' => \Mallto\Admin\Middleware\Authenticate::class,
+        'adminE.auto_permission' => \Mallto\Admin\Middleware\AutoPermissionMiddleware::class,
+        'adminE.log' => \Mallto\Admin\Middleware\OperationLog::class,
+        'adminE.pjax' => Pjax::class,
+        'adminE.replacement_password' => \Mallto\Admin\Middleware\ReplacementPassword::class,
     ];
 
     /**
@@ -53,20 +51,20 @@ class ServiceProvider extends BaseServiceProvider
      * @var array
      */
     protected $middlewareGroups = [
-            'adminE' => [
-                    'admin.auth',
-                    'adminE.pjax',
-                    'admin.bootstrap',
-                    'adminE.auto_permission',
-                    'adminE.log',
-                    'adminE.replacement_password',
-            ],
-            'adminE_base' => [
-                    'admin.auth',
-                    'adminE.pjax',
-                    'admin.bootstrap',
-                    'adminE.log',
-            ],
+        'adminE' => [
+            'admin.auth',
+            'adminE.pjax',
+            'admin.bootstrap',
+            'adminE.auto_permission',
+            'adminE.log',
+            'adminE.replacement_password',
+        ],
+        'adminE_base' => [
+            'admin.auth',
+            'adminE.pjax',
+            'admin.bootstrap',
+            'adminE.log',
+        ],
     ];
 
     /**
@@ -75,10 +73,10 @@ class ServiceProvider extends BaseServiceProvider
      * @var array
      */
     protected $listen = [
-            SubjectSaved::class => [
-                    CreateAdminRole::class,
-                    SubjectCacheClear::class,
-            ],
+        SubjectSaved::class => [
+            CreateAdminRole::class,
+            SubjectCacheClear::class,
+        ],
     ];
 
 
@@ -91,17 +89,17 @@ class ServiceProvider extends BaseServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([__DIR__ . '/../resources/config' => config_path()],
-                    'laravel-admin-enhance-config');
+                'laravel-admin-enhance-config');
 
             $this->publishes([__DIR__ . '/../resources/assets' => public_path('vendor/laravel-adminE')],
-                    'laravel-admin-enhance-assets');
+                'laravel-admin-enhance-assets');
 
             $this->publishes([__DIR__ . '/../resources/file' => public_path('vendor/file')],
-                    'laravel-admin-enhance-assets');
+                'laravel-admin-enhance-assets');
 
             //发布view覆盖laravel-admin的view
             $this->publishes([__DIR__ . '/../resources/admin/views' => resource_path('views/vendor/admin')],
-                    'laravel-admin-enhance-views');
+                'laravel-admin-enhance-views');
             //发布assets覆盖laravel-admin的assets
 //            $this->publishes([__DIR__.'/../resources/admin/assets' => public_path('vendor/laravel-admin')],
 //                'laravel-admin-enhance-assets');
@@ -132,16 +130,13 @@ class ServiceProvider extends BaseServiceProvider
         $this->commands($this->commands);
 
         $this->app->bind(
-                AdminUserUsecase::class,
-                AdminUserUsecaseImpl::class
+            AdminUserUsecase::class,
+            AdminUserUsecaseImpl::class
         );
 
-        if (!Type::hasType('double')) {
-            try {
-                Type::addType('double', FloatType::class);
-            } catch (DBALException $e) {
-            }
-        }
+//        if (!Type::hasType('double')) {
+//                Type::addType('double', FloatType::class);
+//        }
 
         //Admin::booted(function () {
         //    \Mallto\Admin\Facades\AdminE::quickAccess();
@@ -171,7 +166,7 @@ class ServiceProvider extends BaseServiceProvider
     protected function customMorphMap()
     {
         Relation::morphMap([
-                'subject' => Subject::class,
+            'subject' => Subject::class,
         ]);
     }
 
