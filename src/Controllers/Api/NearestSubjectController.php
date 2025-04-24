a9ad1d1dc1f2ede8ef85ccddd6413b61f1afdcbf
never615 <never615@gmail.com>
2025-04-24 10:38:42 +0800

    最近subject 查询 请求参数校验优化

--- src/Controllers/Api/NearestSubjectController.php
+++ src/Controllers/Api/NearestSubjectController.php
@@ -8,6 +8,7 @@ namespace Mallto\Admin\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Mallto\Tool\Exception\ResourceException;
 
 /**
  * 获取最近的主体
@@ -30,12 +31,20 @@ class NearestSubjectController extends Controller
     public function index(Request $request)
     {
         $this->validate($request, [
-            'longitude' => 'required',
-            'latitude'  => 'required',
+            'longitude' => 'required|numeric',
+            'latitude' => 'required|numeric',
         ]);
-
         $longitude = $request->get('longitude');
         $latitude = $request->get('latitude');
+
+        //校验坐标范围是否正常
+        if ($longitude > 180 || $longitude < -180) {
+            throw new ResourceException('longitude is not in range');
+        }
+        if ($latitude > 90 || $latitude < -90) {
+            throw new ResourceException('latitude is not in range');
+        }
+
 
         //获取最近的主体
         $result = DB::select("select name,uuid,
